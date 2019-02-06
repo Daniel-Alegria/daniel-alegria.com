e83d8a7fec4c235c148b9eee9ba45fc068cfe654
Daniel-Alegria <libertatia@protonmail.com>
2019-02-06 11:02:56 -0800

    created layouts directory

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "206017723973aaaa124c";
+/******/ 	var hotCurrentHash = "30333a8eb549f1500269";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
