1edbc6fa2421c6331ad79c13b8feb475ff0a1ebd
Daniel-Alegria <libertatia@protonmail.com>
2019-02-06 12:27:12 -0800

    Rough Draft of Advertisement component

---
+++ components/Advertisement.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+  //TODO: Add variables
+  // - border
+  // - border-radius
+  //TODO: (For all components)
+  // - add grid area
+const MainAdvertisement = styled.div`
+  border: solid 3px #4b4b4b;
+  border-radius: 5px;
+`;
+
+const MainAdvertisement = () => (
+  <MainAdvertisement>
+    Header
+  </MainAdvertisement>
+)
+
+export default MainAdvertisement
