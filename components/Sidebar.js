3ffe499eb485313d43e4fc6b6e10b55ccfc35314
Daniel-Alegria <libertatia@protonmail.com>
2019-02-06 12:22:39 -0800

    Rough Draft of Sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+  //TODO: Add variables
+  // - border
+  // - border-radius
+  //TODO: (For all components)
+  // - add grid area
+const MainSidebar = styled.article`
+  border: solid 3px #4b4b4b;
+  border-radius: 5px;
+`;
+
+const MainSidebar = () => (
+  <MainSidebar>
+    Header
+  </MainContent>
+)
+
+export default MainSidebar
